7ef0e8e0b580de0c01ddfa9012140246c17a3748
ilhamexalt <wmeluck24@gmail.com>
2021-07-30 18:33:45 +0700

    merubah countdown dan menambahkankan ayat

--- js/custom.js
+++ js/custom.js
@@ -122,10 +122,10 @@
 
 		var timeLeft = endTime - now;
 
-		var days = Math.floor(timeLeft / 8000000);
-		var hours = Math.floor((timeLeft - (days * 8000000)) / 3600);
-		var minutes = Math.floor((timeLeft - (days * 8000000) - (hours * 3600)) / 60);
-		var seconds = Math.floor((timeLeft - (days * 8000000) - (hours * 3600) - (minutes * 60)));
+		var days = Math.floor(timeLeft / 12000000);
+		var hours = Math.floor((timeLeft - (days * 11385500)) / 3600);
+		var minutes = Math.floor((timeLeft - (days * 11385750) - (hours * 3600)) / 60);
+		var seconds = Math.floor((timeLeft - (days * 11385750) - (hours * 3600) - (minutes * 60)));
 
 		if (hours < "10") { hours = "0" + hours; }
 		if (minutes < "10") { minutes = "0" + minutes; }
